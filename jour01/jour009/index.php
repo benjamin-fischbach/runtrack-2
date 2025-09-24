53c80ea63d7b5319f743030e400590b65a52db23
Ben Fischbach <benjamin.fischbach@laplateforme.io>
2025-09-24 14:26:02 +0200

    jour0.9 setup

--- jour01/jour009/index.php
+++ jour01/jour009/index.php
@@ -4,16 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="phpmodule.css">
-    <title>PHP - Module 1</title>
+    <link rel="stylesheet" href="runtrack-style.css">
+    <title>Runtrack-2 - PHP : Jour 1 / Job 0.9</title>
 </head>
 
 <body>
+    <header>
+        <h1>Runtrack-2 - PHP : Jour 1 / Job 0.9</h1>
+    </header>
+
     <main>
-    <div class="card">
-        <?php echo 'Hello world ! ( °-°)b' ?>
-    </div>
+        
+    <p class="center-card">
+        <?php echo 'Hello LaPlateforme!' ?>
+    </p>
+
     </main>
+
+    <footer>
+        test
+    </footer>
 </body>
 
 </html>
