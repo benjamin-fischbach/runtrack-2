e1c2ff599ac5f0eea9dd28211665f991ff198917
Ben Fischbach <benjamin.fischbach@laplateforme.io>
2025-09-25 14:34:26 +0200

    jour 1 job 02 done

---
+++ jour01/job02/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/runtrack2/assets/css/runtrack-style.css">
+    <title>Runtrack-2 - PHP : Jour 1 / Job 2</title>
+</head>
+
+
+<body>
+    <header>
+        <h1>Runtrack-2 - PHP : Jour 1 / Job 2</h1>
+    </header>
+    <main>
+    <h3 class="title-bar">Espace PHP</h3>  
+    <p class="center-card">
+
+        
+<?php
+echo 'Hello LaPlateforme!<br>'; // Ceci est un commentaire en ligne
+
+echo 'Hello LaPlateforme!<br>'; 
+/* Ceci est un commentaire 
+en bloc */
+?>
+
+
+
+    </p>
+    </main>
+
+    <footer>
+        Ben Fischbach - CDPI La Plateforme, Marseille - 2025-2026
+    </footer>
+</body>
+
+</html>
