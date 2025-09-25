c499d07507f1cfa32cfe700355a9b3849ad64f3a
Ben Fischbach <benjamin.fischbach@laplateforme.io>
2025-09-25 09:28:48 +0200

    add a stylesheet for presentation

--- jour01/jour00/index.php
+++ jour01/jour00/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="runtrack-style.css">
+    <link rel="stylesheet" href="/runtrack2/assets/css/runtrack-style.css">
     <title>Runtrack-2 - PHP : Jour 1 / Job 0.9</title>
 </head>
 
