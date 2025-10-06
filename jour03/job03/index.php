39e74e24fa7741fc16466fdb2f809dacbc78064c
Ben Fischbach <benjamin.fischbach@laplateforme.io>
2025-10-06 09:57:23 +0200

    jour03-job03 done

--- jour03/job03/index.php
+++ jour03/job03/index.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/runtrack2/assets/css/runtrack-style.css">
-    <title>Runtrack-2 - PHP : Jour 3 / Job 2</title>
+    <title>Runtrack-2 - PHP : Jour 3 / Job 3</title>
 </head>
 
 <body>
     <header>
-        <h1>Runtrack-2 - PHP : Jour 3 / Job 2</h1>
+        <h1>Runtrack-2 - PHP : Jour 3 / Job 3</h1>
     </header>
     <main>
     <h3 class="title-bar">Espace PHP</h3>  
@@ -19,27 +19,11 @@
 <div class="elseif">
 
 <?php
-$str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie";
-$obj1 = "";
+$str = "I'm sorry Dave I'm afraid I can't do that";
+$obj1 = str_ireplace(array('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','z','\'',' '), '', $str);
 
-for($i = 0; $i < strlen($str); $i += 2) $obj1 .= $str[$i];
+echo $obj1;
 
-echo "$obj1.";
-
-/*  .=  sign reassembles the sentence
-
-$var = 1;
-$var .= 20;
-
-means
-
-$var = 1 . 20;
-
-wich gives
-
-$var = 120;
-
-*/
 
 ?>
 
