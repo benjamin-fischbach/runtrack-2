3b4553479d59160f3358ea5a6642728f27dcde7c
Ben Fischbach <benjamin.fischbach@laplateforme.io>
2025-11-17 16:55:28 +0100

    Jour 08 Job 04 css connexion

---
+++ jour08/job04/index.php
@@ -0,0 +1,47 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/runtrack2/assets/css/runtrack-style.css">
+    <title>Runtrack-2 - PHP : Jour 8 / Job 4</title>
+</head>
+
+<body>
+    <header>
+        <h1>Runtrack-2 - PHP : Jour 8 / Job 4</h1>
+    </header>
+    <main>
+    <h3 class="title-bar">Espace PHP</h3>  
+    <div class="center-card">
+
+<div class="elseif">
+
+<div class="connection">
+    <h2>Connexion</h2>
+<form method="post" action="index.php">
+    <label for="prenom">Prénom:</label><br>
+    <input type="text" name="prenom" class="prenom"/>
+    <input type="submit" name="submit" class="submit" value="Connexion" />
+</form>
+</div>
+
+
+
+</div>
+</div>
+    </main>
+
+    <footer>
+        Ben Fischbach - CDPI La Plateforme, Marseille - 2025-2026
+    </footer>
+</body>
+
+</html>
