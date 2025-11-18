f3af79e2fa62d1b9f14d020a8ffa3152ad490f66
Ben Fischbach <benjamin.fischbach@laplateforme.io>
2025-11-18 10:47:52 +0100

    Jour 08 Job 04 done

--- jour08/job04/index.php
+++ jour08/job04/index.php
@@ -1,6 +1,32 @@
 <?php
 
+// retenir l'email de la personne connectée pendant 1 an
 
+//setcookie(
+
+//    'LOGGED_USER',
+
+//    'utilisateur@exemple.com',
+
+//    [
+
+//        'expires' => time() + 365*24*3600,
+
+//        'secure' => true,
+
+//        'httponly' => true,
+
+//    ]
+
+//);
+//?>
+
+
+<?php
+
+if(!isset($_COOKIE['prenom'])){
+    setcookie('prenom');
+}
 ?>
 
 
@@ -24,15 +50,47 @@
 
 <div class="elseif">
 
+
+<?php
+    if (!isset($_POST['connexion'])) {
+?>
+
 <div class="connection">
     <h2>Connexion</h2>
 <form method="post" action="index.php">
     <label for="prenom">Prénom:</label><br>
     <input type="text" name="prenom" class="prenom"/>
-    <input type="submit" name="submit" class="submit" value="Connexion" />
+    <input type="submit" name="connexion" class="connexion" value="connexion" />
 </form>
 </div>
 
+<?php
+    }
+    else {
+        // form engine
+        echo "<div></div>";
+    }
+?>
+
+<?php
+//si le bouton "Connexion" est cliqué
+if(isset($_POST['connexion'])){
+  // on vérifie que le champ "prenom" n'est pas vide
+  // empty vérifie à la fois si le champ est vide et si le champ existe belle et bien (is set)
+  if(empty($_POST['prenom'])){
+    echo "Veuillez entrer un prénom.";
+  }else{
+          $_COOKIE['prenom'] = $_POST['prenom'];
+          $prenom = $_COOKIE['prenom'];
+          echo "Bonjour ". $prenom."  !";
+          echo "<form method=\"post\" action=\"index.php\"><input type=\"submit\" name=\"deconnexion\" class=\"deconnexion\" value=\"deconnexion\" /></form>";
+        }
+  }
+if(isset($_POST['deconnexion'])){
+unset($_COOKIE['prenom']);
+header("index.php");
+}
+?>
 
 
 </div>
